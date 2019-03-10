63d94930878a907769ea8c962e23b2ef4fae8c6c
Usman Bashir <me@usmanbashir.com>
2019-03-10 14:55:43 +0300

    Code Along Source

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  // On Class Load set Default State
+  state = { toggle: false };
+
   render() {
+    let onOrOff = '';
+
+    if(this.state.toggle === true) {
+      onOrOff = "ON";
+    } else {
+      onOrOff = "OFF";
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <button onClick={this.toggleButton}>{ onOrOff }</button>
     );
+  }
+
+  toggleButton = () => {
+    if(this.state.toggle === true) {
+      this.setState( { toggle: false } );
+    } else {
+      this.setState( { toggle: true } );
+    }
   }
 }
 
